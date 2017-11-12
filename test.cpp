e1f8dd0257817fa02c84117e32b8fd1948285ec0
inknos <nicola.sella@outlook.it>
2017-11-12 21:25:21 +0100

    algoritmo non funziona

--- test.cpp
+++ test.cpp
@@ -1,8 +1,30 @@
 //
 //  test.cpp
-//  
+//
 //
 //  Created by Federico on 12/11/17.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include "Lattice.h"
+
+using namespace std;
+
+void test(){
+  Lattice uni(5,1,2);
+  Lattice bi(3,2,4);
+  Lattice tri(3,3,6);
+
+
+
+  uni.printLattice();
+  cout << endl;
+  bi.printLattice();
+  cout << endl;
+  tri.printLattice();
+  cout << endl;
+
+  cout << uni.energy() << endl;
+  cout << bi.energy() << endl;
+  cout << tri.energy() << endl;
+}
