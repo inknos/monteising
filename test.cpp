706470e29d2d81dc6b0a94d6313020ea11da9297
MrFrankCin <federico.cinus@gmail.com>
2017-11-13 17:49:10 +0100

    threads prova

--- test.cpp
+++ test.cpp
@@ -8,16 +8,18 @@
 #include <iostream>
 #include "Lattice.h"
 #include "TRandom3.h"
+#include "TStopwatch.h"
 
 using namespace std;
 
 void test(){
+  TStopwatch timer;
   Lattice uni(5,1,2);
   Lattice bi(3,2,4);
   Lattice tri(3,3,6);
   Lattice uui(5,1,2);
   Lattice bbi(3,2,4);
-  Lattice ttri(3,3,6);
+  Lattice ttri(100,3,6);
 
   cout << uni << endl;
   cout << uui << endl;
@@ -26,14 +28,28 @@ void test(){
   cout << "tre\n";
   cout << tri << endl;
   cout << "tre bis\n";
-  cout << ttri<< endl;
+  //cout << ttri<< endl;
   
+  timer.Start();
   cout << uni.energy() << endl;
   cout << uui.energy() << endl;
   cout << bi.energy() << endl;
   cout << bbi.energy()<< endl;
   cout << tri.energy() << endl;
   cout <<ttri.energy() << endl;
-
-  cout << "seed" << gRandom->GetSeed(); << endl;
+  timer.Stop();
+  
+  cout << "seed" << gRandom->GetSeed() << endl;
+  timer.Print();
 }
+
+
+
+
+
+
+
+
+
+
+
