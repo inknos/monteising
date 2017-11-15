dde15a7b8c011620e8a8c164daedea5a78f47b42
inknos <nicola.sella@outlook.it>
2017-11-15 20:22:38 +0100

    corretto seed parallelTest.cpp

--- parallelTest.cpp
+++ parallelTest.cpp
@@ -7,7 +7,9 @@
 using namespace std;
 
 void parallelTest(){
+
   gRandom -> SetSeed(4357);
+
   int nn = 0;
   int tt = 4;
   cout << "Lattice N: ";
@@ -21,9 +23,10 @@ void parallelTest(){
   Lattice a(nn,1,2);
   Lattice b(nn,2,4);
   Lattice c(nn,3,6);
-  Lattice aP(nn,1,2);
-  Lattice bP(nn,2,4);
-  Lattice cP(nn,3,6);
+  
+  //Lattice aP(nn,1,2);
+  //Lattice bP(nn,2,4);
+  //Lattice cP(nn,3,6);
 
   cout << "1-D, " << nn << " entries, seq.\n";
   timer.Start();
@@ -48,21 +51,21 @@ void parallelTest(){
 
   cout << "1-D, " << nn << " entries, "<< tt << " threads\n";
   timer.Start();
-  cout << aP.energyParallel(tt)<< endl;
+  cout << a.energyParallel(tt)<< endl;
   timer.Stop();
   timer.Print();
   cout << endl;
 
   cout << "2-D, " << nn << " entries, " << tt << " threads\n";
   timer.Start();
-  cout << bP.energyParallel(tt) << endl;
+  cout << b.energyParallel(tt) << endl;
   timer.Stop();
   timer.Print();
   cout << endl;
 
   cout << "3-D, " << nn << " entries, "<< tt << " threads\n";
   timer.Start();
-  cout << cP.energyParallel(tt) << endl;
+  cout << c.energyParallel(tt) << endl;
   timer.Stop(); 
   timer.Print();
   cout << endl;
