997df9f91eabc7a11db944670f2a1e0f79dab41a
inknos <nicola.sella@outlook.it>
2017-12-01 16:43:47 +0100

    todo

---
+++ TODO.md
@@ -0,0 +1,7 @@
+# TODO
+
+- struttturare i file come tree di temperatura ove ogni reticolo e` un branch
+
+- eliminare tclonesarray
+
+- suscettivita` termica e calore specifico
--- simulateAnalyzePlot.C
+++ simulateAnalyzePlot.C
@@ -5,7 +5,7 @@
 
 
 void simulateAnalyzePlot(int n = 10, int dim = 2, int dim_v = 10, const char* nn = "test_cooling.root",
-			 int i0 = 100000, int iter = 1000000, double t1 = 0., double t2 = 4., int step = 20 ){
+			 int i0 = 10000, int iter = 100000, double t1 = 0., double t2 = 4., int step = 20 ){
   TStopwatch timer;
 
   SimulationLattice s(n, dim, dim_v, nn, i0, iter, t1, t2, step);
