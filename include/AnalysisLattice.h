1e9d6383542b99997ddc9ff8537ff72704b26fe3
inknos <nicola.sella@outlook.it>
2017-12-11 13:02:37 +0100

    merge

--- include/AnalysisLattice.h
+++ include/AnalysisLattice.h
@@ -43,25 +43,31 @@ class AnalysisLattice {
   TString file_in;
   TString file_out;
   static double TempCritic;
-  
 
   std::vector<double> bin(const std::vector<double>& vec);
-
   std::vector<double> binN(cuint& num_bin, const std::vector<double>& vec);
-  
+
  public:
- 
-  static double analiticX(double * x, double * par);
-  static double analiticM(double * x , double * par);
+
+  AnalysisLattice(const TString& file_input, const TString& file_output);
+
+  void run();
 
   TGraphErrors * drawLattice(cuint& lattice_number,
                              cuint& x_axis,
                              cuint& y_axis);
-                             
+
   TGraphErrors * drawLatticeMean(cuint& x_axis,
                                  cuint& y_axis);
-  
-  
+
+  TMultiGraph * draw(cuint& x_axis, cuint& y_axis);
+
+
+  static double analiticX(double * x, double * par);
+  static double analiticM(double * x , double * par);
+
+  void findTcritic();
+
   void fitLattice         ( bool mean,
                             cuint& x_axis,
                             cuint& y_axis,
@@ -70,10 +76,11 @@ class AnalysisLattice {
                             double fit_temp_max,
                             int lat_number
                           );
-          
-  void plotAnalitic(); 
-                               
-  AnalysisLattice(const TString& file_input, const TString& file_output);
+
+
+  static double getTempCritic();
+
+  static void setTempCritic(const double& _TempCritic);
 
   TString getFileIn() const;
 
@@ -82,16 +89,10 @@ class AnalysisLattice {
   void setFileIn(const TString& file_input);
 
   void setFileOut(const TString& file_output);
-  
-  static double getTempCritic();
-    
-  static void setTempCritic(const double& _TempCritic);
-  
-  void run();
 
-  TMultiGraph * draw(cuint& x_axis, cuint& y_axis);
-  
-  void findTcritic();
+
+
+
 };
 
 #endif
