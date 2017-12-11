91891b6e16273eeb68d0a1069dc1e3215e14b583
inknos <nicola.sella@outlook.it>
2017-12-11 13:08:04 +0100

    simulation

--- src/SimulationLattice.cxx
+++ src/SimulationLattice.cxx
@@ -176,7 +176,7 @@ void SimulationLattice::run(){
     temp_array.push_back( 0.);
   }
   double tc = 2.27 + gRandom -> Gaus(0, 0.1);
-  double vc = 1;
+  double vc = 0.5;
   for(uint t = 0; t < tempstep / 4; t++){
     temp_array[ t * 4 + 0 ] =  tempmin + ( tc - vc - tempmin ) * ( (double) t / ((double)tempstep / 4) );
     temp_array[ t * 4 + 1 ] =  tc - vc + vc  * ( (double) t / ((double)tempstep / 4) );
